dcb69f61f0bcac9ebc5d0bd296d8597ebb5d46a1
Joe Dallago <jd.dallago@gmail.com>
2011-04-10 13:00:22 -0500

    Deleted all of the initial classes to create the standard stylesheet.

--- studentunderground/handlers/test.py
+++
@@ -1,7 +0,0 @@
-from pyramid_handlers import action
-from studentunderground.handlers.handler import Handler
-
-class TestHandler(Handler):
-    @action(renderer='layouts/test.mako')
-    def style(self):
-        return {}
--- studentunderground/security.py
+++
@@ -1,6 +0,0 @@
-USERS = {'jayd3e':'sharp7&7'}
-GROUPS = {'jayd3e':['group:admins']}
-
-def groupfinder(userid, request):
-    if userid in USERS:
-        return GROUPS.get(userid, [])
