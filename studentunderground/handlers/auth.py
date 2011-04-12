b05f7cc6cd139cd4b7ef406fb59ad7552b6d6cd1
Joe Dallago <jd.dallago@gmail.com>
2011-04-12 09:26:58 -0500

    Starting completely ane

--- studentunderground/handlers/auth.py
+++
@@ -1,7 +0,0 @@
-class AuthHandler(object):
-    """
-    This handler handles all of the authentication functionality.
-    """
-
-    def __init__(self, request):
-        self.request = request
--- studentunderground/models/site.py
+++
@@ -1,5 +0,0 @@
-class SiteModel(object):
-    '''Root object, more information to be inserted later'''
-
-    def __init__(self, request):
-        pass
