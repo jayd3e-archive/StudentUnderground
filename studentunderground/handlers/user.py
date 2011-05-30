cb271fb1e5e4e945961eec112c4041b99d66f2b9
Joe Dallago <jd.dallago@gmail.com>
2011-05-30 00:55:16 -0500

    Added a user handler.

---
+++ studentunderground/handlers/user.py
@@ -0,0 +1,15 @@
+from pyramid_handlers import action
+
+class UserHandler(object):
+
+    def __init__(self, request):
+        self.request = request
+        self.here = request.environ['PATH_INFO']
+
+    @action(renderer='user/index.mako')
+    def index(self):
+        title = "S2S | User"
+        legend = "User"
+        return {'here':self.here,
+                'title':title,
+                'legend':legend}
